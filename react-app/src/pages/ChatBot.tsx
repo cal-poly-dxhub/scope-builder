ed0f4bdfa6be89a877e7f84250dcd09f9afeabb6
RyanGertz <ryan.gertz1@gmail.com>
2024-08-19 20:01:35 +0000

    basic chat functionality

--- react-app/src/pages/ChatBot.tsx
+++ react-app/src/pages/ChatBot.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import useBedrockWebSocket from "../hooks/useBedrockWebSocket"
 import "../styles/ChatBot.css";
 
 interface Message {
@@ -9,12 +10,39 @@ interface Message {
 const ChatBot: React.FC = () => {
   const [message, setMessage] = useState<string>("");
   const [messages, setMessages] = useState<Message[]>([]);
+  const {startBedrockProcess, lastJsonMessage, connectionStatus} = useBedrockWebSocket();
+  
+  useEffect(()=>{
+    console.log(message)
+  }, [message])
+  
+  const handleSubmit = (userInput: string) => {
+    startBedrockProcess(userInput);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: "Awaiting Response...", sender: "bot" }
+    ]);
+  };
+  
+  useEffect(() =>{
+    console.log(lastJsonMessage)
+    
+    if (lastJsonMessage?.Payload?.body){
+      const parsedBody = JSON.parse(lastJsonMessage.Payload.body);
+      console.log(parsedBody.search_answer)
+      setMessages((prevMessages) => [
+        ...prevMessages.filter((msg) => msg.text !== "Awaiting Response..."),
+        { text: parsedBody.search_answer, sender: "bot" }
+      ]);
+    }
+  },[lastJsonMessage])
 
   return (
     <div className="chat-app">
       <div className="chat-container">
         <div className="chat-header">
           <h2>react-lambda-chatbot</h2>
+          <h3>Connection Status - {connectionStatus}</h3>
         </div>
         <div className="chat-messages">
           {messages.map((message, index) => (
@@ -37,11 +65,12 @@ const ChatBot: React.FC = () => {
             onKeyDown={(e) => {
               if (e.key === "Enter") {
                 setMessages([...messages, { text: message, sender: "user" }]);
+                handleSubmit(message)
                 setMessage("");
               }
             }}
           />
-          <button>
+          <button onClick={() => handleSubmit(message)}>
             <i className="fa fa-paper-plane"></i>
           </button>
         </div>
